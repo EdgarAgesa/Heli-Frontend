565f394f1b457142a92cce3741598dba18c1eb58
EdgarAgesa <edgarasige@gmail.com>
2025-03-09 02:24:43 +0300

    Log in

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../css/footer.css";
-import { FaTwitter, FaFacebookF, FaYoutube, FaInstagram, FaTripadvisor } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
   return (
